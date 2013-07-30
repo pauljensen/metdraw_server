3ac62c3d9dafed85ea607cdd308f36dfc370c032
Paul Jensen <paj2s@virginia.edu>
2013-07-30 15:36:36 -0400

    async bug in render.js

--- metdraw/routes/render.js
+++ metdraw/routes/render.js
@@ -8,21 +8,24 @@ exports.render = function(req, res) {
     util.write_local_json(sessionid, "METDRAW_STATUS.json", {running: true, outputby: "write local"});
     console.log("finished writing status file");
 
+    var main_run = function() {
+        util.metdraw(sessionid, cmd + ' > output.txt',
+            function(error, stdout, stderr) {
+                console.log('finished main run');
+                console.log(error);
+                console.log(stderr);
+                util.write_local_json(sessionid, "METDRAW_STATUS.json", {running: false, outputby: "callback"});
+                console.log('finished updating status file');
+            });
+    }
+
     util.metdraw(sessionid, '--norun --show ' + cmd,
                  function(error, stdout, stderr) {
                      console.log('finished pre-run');
                      res.write(stdout);
                      res.write(stderr);
                      res.end();
-                 });
-
-    util.metdraw(sessionid, cmd + ' > output.txt',
-                 function(error, stdout, stderr) {
-                     console.log('finished main run');
-                     console.log(error);
-                     console.log(stderr);
-                     util.write_local_json(sessionid, "METDRAW_STATUS.json", {running: false, outputby: "callback"});
-                     console.log('finished updating status file');
+                     main_run();
                  });
 
     //util.write_local_json(sessionid, 'METDRAW_STATUS.json', {running: true});
